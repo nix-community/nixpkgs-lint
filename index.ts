0c719f6bd428d1683f418521e98017ee31b0ec44
Ben Siraphob <bensiraphob@gmail.com>
2022-04-04 14:53:30 -0500

    Refactor with union types

--- index.ts
+++ index.ts
@@ -86,17 +86,18 @@ function walkCursor(cursor: Parser.TreeCursor, p: QueryPred) {
   return walkCursorRec(cursor, 0, p, [] as Parser.SyntaxNode[]);
 }
 
+type QueryPredObj = { query: Parser.Query; pred: QueryPred };
+
 // Combining a query with a predicate
-const mkQueryPred = (query: string, pred: QueryPred) => ({
-  query: new Query(Nix, query),
-  pred: pred,
-});
+function mkQueryPred(query: string, pred: QueryPred): QueryPredObj {
+  return {
+    query: new Query(Nix, query),
+    pred: pred,
+  };
+}
 
 // Query the tree then walk with a predicate
-function queryThenWalk(
-  tree: Parser.Tree,
-  queryPred: { query: Parser.Query; pred: QueryPred }
-) {
+function queryThenWalk(tree: Parser.Tree, queryPred: QueryPredObj) {
   return queryPred.query
     .captures(tree.rootNode)
     .filter((x: Parser.QueryCapture) => x.name == "q")
@@ -114,76 +115,66 @@ function queryThenWalk(
 const matchIdent = (t: string) => (x: Parser.TreeCursor) =>
   x.nodeType == "identifier" && x.nodeText == t;
 
-const matchIdentRegex = (t) => (x) =>
-  x.nodeType == "identifier" && x.nodeText.match(t);
+const matchIdentRegex = (t: RegExp) => (x: Parser.TreeCursor) =>
+  x.nodeType == "identifier" && t.test(x.nodeText);
 
-async function runNew(queryPred: { query: Parser.Query; pred: QueryPred }) {
+async function runNew(
+  x: { query: Parser.Query; pred: QueryPred } | Parser.Query
+) {
   process.stdin.resume();
   const parser = new Parser();
   parser.setLanguage(Nix);
   let files = recurseDir(nixpkgsPath, []);
-  await Promise.allSettled(
-    files.map(async (file) => {
-      const tree = parser.parse(await fs.readFile(file, "utf8"));
-      let l = queryThenWalk(tree, queryPred);
-      if (l.length > 0) {
-        Promise.all(
-          l.map((m) => {
-            console.log(
-              `${file}:${m.start.row + 1} (${m.start.row + 1},${
-                m.start.column + 1
-              })-(${m.end.row + 1},${m.end.column + 1})`
-            );
-          })
-        );
-      }
-    })
-  );
+  if ("pred" in x) {
+    await Promise.allSettled(
+      files.map(async (file) => {
+        const tree = parser.parse(await fs.readFile(file, "utf8"));
+        let l = queryThenWalk(tree, x);
+        if (l.length > 0) {
+          Promise.all(
+            l.map((m) => {
+              console.log(
+                `${file}:${m.start.row + 1} (${m.start.row + 1},${
+                  m.start.column + 1
+                })-(${m.end.row + 1},${m.end.column + 1})`
+              );
+            })
+          );
+        }
+      })
+    );
+  } else {
+    await Promise.allSettled(
+      files.map(async (file) => {
+        const tree = parser.parse(await fs.readFile(file, "utf8"));
+        let l = capturesByName(tree, x, "q");
+        if (l.length > 0) {
+          console.log(file + ":" + (l[0].row + 1));
+        }
+      })
+    );
+  }
   process.exit(0);
 }
 
-async function runOld(query: Parser.Query) {
-  process.stdin.resume();
-  const parser = new Parser();
-  parser.setLanguage(Nix);
-  let files = recurseDir(nixpkgsPath, []);
-  await Promise.allSettled(
-    files.map(async (file) => {
-      const tree = parser.parse(await fs.readFile(file, "utf8"));
-      let l = capturesByName(tree, query, "q");
-      if (l.length > 0) {
-        console.log(file + ":" + (l[0].row + 1));
-      }
-    })
-  );
-  process.exit(0);
-}
-
-// TODO: refactor
-
-// Currently a bit haphazard.  I used to only rely on the query engine
-// but it was matching false positives such as comments.
-const prompt = new Select({
-  name: "query",
-  message: "What anti-pattern do you want to debug?",
-  choices: [
-    {
-      message: "pkg-config in buildInputs",
-      value: mkQueryPred(
-        `((binding attrpath: _ @a expression: _ @l) (#eq? @a "buildInputs") (#match? @l "pkg-config")) @q`,
-        matchIdent("pkg-config")
-      ),
-    },
-    {
-      message: "cmake in buildInputs",
-      value: mkQueryPred(
-        `((binding attrpath: _ @a expression: _ @l) (#eq? @a "buildInputs") (#match? @l "cmake")) @q`,
-        matchIdent("cmake")
-      ),
-    },
-    {
-      message: "dontBuild = true in stdenv.mkDerivation",
-      value: `
+const choices: { message: string; value: string | QueryPredObj }[] = [
+  {
+    message: "pkg-config in buildInputs",
+    value: mkQueryPred(
+      `((binding attrpath: _ @a expression: _ @l) (#eq? @a "buildInputs") (#match? @l "pkg-config")) @q`,
+      matchIdent("pkg-config")
+    ),
+  },
+  {
+    message: "cmake in buildInputs",
+    value: mkQueryPred(
+      `((binding attrpath: _ @a expression: _ @l) (#eq? @a "buildInputs") (#match? @l "cmake")) @q`,
+      matchIdent("cmake")
+    ),
+  },
+  {
+    message: "dontBuild = true in stdenv.mkDerivation",
+    value: `
 ((apply_expression
     function: _ @b
     argument: [(rec_attrset_expression
@@ -197,40 +188,39 @@ const prompt = new Select({
  (#match? @a "dontBuild")
  (#match? @e "true")) @q
 `,
-    },
-    {
-      message: "redundant packages from stdenv in nativeBuildInputs",
-      value: mkQueryPred(
-        `
+  },
+  {
+    message: "redundant packages from stdenv in nativeBuildInputs",
+    value: mkQueryPred(
+      `
     ((binding attrpath: _ @a expression: _ @i)
     (#eq? @a "nativeBuildInputs")
     (#match? @i "coreutils|findutils|diffutils|gnused|gnugrep|gawk|gnutar|gzip|bzip2\.bin|gnumake|bash|patch|xz\.bin"))
      @q`,
-        matchIdentRegex(
-          /^(coreutils|findutils|diffutils|gnused|gnugrep|gawk|gnutar|gzip|bzip2\.bin|gnumake|bash|patch|xz\.bin)$/
-        )
-      ),
-    },
-    {
-      message: "pytestCheckHook in checkInputs",
-      value: mkQueryPred(
-        `((binding attrpath: _ @a expression: _ @l) (#eq? @a "checkInputs") (#match? @l "pytestCheckHook")) @q`,
-        matchIdent("pytestCheckHook")
-      ),
-    },
-  ],
-  result: (x) => (x instanceof Object ? { q: x } : { s: x }),
-  format: (_) => "",
+      matchIdentRegex(
+        /^(coreutils|findutils|diffutils|gnused|gnugrep|gawk|gnutar|gzip|bzip2\.bin|gnumake|bash|patch|xz\.bin)$/
+      )
+    ),
+  },
+  {
+    message: "pytestCheckHook in checkInputs",
+    value: mkQueryPred(
+      `((binding attrpath: _ @a expression: _ @l) (#eq? @a "checkInputs") (#match? @l "pytestCheckHook")) @q`,
+      matchIdent("pytestCheckHook")
+    ),
+  },
+];
+const prompt = new Select({
+  name: "query",
+  message: "What anti-pattern do you want to debug?",
+  choices: choices,
+  result: (x: QueryPredObj | string) =>
+    typeof x === "string" ? new Query(Nix, x) : x,
+  format: (_: QueryPredObj | string) => "",
 });
 
-async function runDispatch(
-  r: { q: { query: Parser.Query; pred: QueryPred } } | { s: string }
-) {
-  if ("q" in r) {
-    await runNew(r.q);
-  } else {
-    await runOld(new Query(Nix, r.s));
-  }
+async function runDispatch(x: QueryPredObj | Parser.Query) {
+  await runNew(x);
 }
 
 prompt.run().then(runDispatch).catch(console.error);
